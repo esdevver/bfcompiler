bc584825545bcd2125580476d520ce7adb365a2b
esdevver <esdevver@gmail.com>
2022-02-27 04:12:48 +0000

    feat: display visual of AST in CLI

--- src/comp.c
+++ src/comp.c
@@ -101,6 +101,66 @@ STATUS read_brainf(FILE *source, vector *instruction_list) {
   return STATUS_EOF;
 }
 
+#define PRETTY_INSTRUCTION(COLOR, INSTRUCTION)                                 \
+  printf("\x1b[38;5;%sm%s\x1b[0m\n", COLOR, INSTRUCTION)
+#define PRETTY_INSTRUCTION_COUNT(COLOR, INSTRUCTION, COUNT)                    \
+  printf("\x1b[38;5;%sm%s\x1b[0m \x1b[38;5;166m%d\x1b[0m\n", COLOR,            \
+         INSTRUCTION, COUNT)
+
+void print_syntax(vector *instruction_list, int indent) {
+  instruction_unit *instruction;
+  instruction_unit_count *move;
+  instruction_unit_count *add;
+  size_t i;
+  int j;
+  for (i = 0; i < instruction_list->length; i++) {
+    instruction = vector_get(instruction_list, i);
+    for (j = 0; j < indent; j++)
+      printf("  ");
+    switch (instruction->type) {
+    case INSTRUCTION_LOOP:
+      PRETTY_INSTRUCTION("141", "LOOP");
+      print_syntax(&((instruction_unit_list *)instruction)->list, indent + 1);
+      break;
+    case INSTRUCTION_MOVE:
+      move = (instruction_unit_count *)instruction;
+      if (move->count == 0)
+        PRETTY_INSTRUCTION("246", "NOP");
+      else if (move->count > 0) {
+        if (move->count == 1)
+          PRETTY_INSTRUCTION("167", "RIGHT");
+        else
+          PRETTY_INSTRUCTION_COUNT("167", "RIGHT", move->count);
+      } else {
+        if (move->count == -1)
+          PRETTY_INSTRUCTION("167", "LEFT");
+        else
+          PRETTY_INSTRUCTION_COUNT("167", "LEFT", -move->count);
+      }
+      break;
+    case INSTRUCTION_ADD:
+      add = (instruction_unit_count *)instruction;
+      if (add->count == 0)
+        PRETTY_INSTRUCTION("246", "NOP");
+      else if (add->count <= 128) {
+        PRETTY_INSTRUCTION_COUNT("221", "ADD", add->count);
+      } else {
+        PRETTY_INSTRUCTION_COUNT("221", "SUB", 256 - add->count);
+      }
+      break;
+    case INSTRUCTION_OUT:
+      PRETTY_INSTRUCTION("116", "OUT");
+      break;
+    case INSTRUCTION_IN:
+      PRETTY_INSTRUCTION("116", "IN");
+      break;
+    }
+  }
+}
+
+#undef PRETTY_INSTRUCTION
+#undef PRETTY_INSTRUCTION_COUNT
+
 int main(int argc, char *argv[]) {
 
   FILE *source;
@@ -128,6 +188,8 @@ int main(int argc, char *argv[]) {
   vector_reserve(&instruction_list, 4);
 
   read_brainf(source, &instruction_list);
+
+  print_syntax(&instruction_list, 0);
 
   return 0;
 }
